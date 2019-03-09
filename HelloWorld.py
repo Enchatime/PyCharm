1c9a67793c0000e5b73a0405870433c33aac9f6c
Enchatime <enchatime@icloud.com>
2019-03-09 23:03:29 +0300

    changes in code again

--- HelloWorld.py
+++ HelloWorld.py
@@ -2,3 +2,10 @@ def HelloWorld(name):
     print('Hello world ' + name + '!')
 
 HelloWorld('tester')
+
+def numbers():
+    numberslist=[1, 2, 3, 4, 5, 6]
+    for number in numberslist:
+        print(number)
+
+numbers()
